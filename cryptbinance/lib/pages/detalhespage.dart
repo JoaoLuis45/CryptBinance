d4f7ca16371936ebea68d6f7130caf952d7dc210
Joaoluis45 <joaoluisramos45@gmail.com>
2025-01-21 16:56:05 -0300

    feat: finish buy Crypto

--- cryptbinance/lib/pages/detalhespage.dart
+++ cryptbinance/lib/pages/detalhespage.dart
@@ -1,5 +1,6 @@
 import 'package:cryptbinance/models/moeda.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 
 class Detalhespage extends StatefulWidget {
@@ -13,7 +14,19 @@ class Detalhespage extends StatefulWidget {
 class _DetalhespageState extends State<Detalhespage> {
   final _form = GlobalKey<FormState>();
   final _valor = TextEditingController();
+  double quantidade = 0;
   NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+
+  comprar() {
+    if (_form.currentState!.validate()) {
+      Navigator.pop(context);
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Compra realizada com sucesso!')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,16 +67,86 @@ class _DetalhespageState extends State<Detalhespage> {
                 ],
               ),
             ),
+            (quantidade > 0)
+                ? SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Container(
+                      child: Text(
+                        '$quantidade ${widget.moeda.sigla}',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.teal,
+                        ),
+                      ),
+                      margin: EdgeInsets.only(bottom: 24),
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: Colors.teal.withOpacity(0.2),
+                      ),
+                    ),
+                  )
+                : Container(
+                    padding: EdgeInsets.only(bottom: 24),
+                  ),
             Form(
               key: _form,
               child: TextFormField(
                 controller: _valor,
                 style: TextStyle(fontSize: 22),
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'Valor',
                   prefixIcon: Icon(Icons.monetization_on_outlined),
-                  suffix: Text('BRL')
+                  suffix: Text('BRL'),
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Informe o valor da compra.';
+                  } else if (double.parse(value) < 50) {
+                    return 'O valor de compra mínimo é R\$50,00';
+                  }
+
+                  return null;
+                },
+                onChanged: (value) {
+                  setState(() {
+                    quantidade = (value.isEmpty)
+                        ? 0
+                        : double.parse(value) / widget.moeda.preco;
+                  });
+                },
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              margin: EdgeInsets.only(top: 24),
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.blue)),
+                onPressed: comprar,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.check,
+                      color: Colors.white,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(12),
+                      child: Text(
+                        'Comprar',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ),
             )
